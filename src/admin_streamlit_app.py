79db91bbfc1a6f0a8a3bc70f398c7a4b9ddd8caf
Dennis Bakhuis <git@bakhuis.nu>
2024-11-19 15:50:55 +0100

    finished admin panel

--- src/admin_streamlit_app.py
+++ src/admin_streamlit_app.py
@@ -1,10 +1,14 @@
 """Advanced Analytics scavenger hunt application."""
 from pathlib import Path
+import json
 
 import streamlit as st
+import pandas as pd
+import plotly.express as px
+from geopy.distance import geodesic
 
 from models import State, Game, Location
-from constants import STATE_FILE, GAME_FILE
+from constants import STATE_FILE, GAME_FILE, LOGGING_FILE
 
 
 # Get game files
@@ -21,14 +25,34 @@ if "index" not in st.session_state:
 #############
 def scavenger_admin():
     """Scavenger hunt admin interface."""
-    st.title("Scavenger hunt ADMIN 🕵")
-    overview_tab, questions_tab, about_tab = st.tabs(["Overview", "Questions", "About"])
+    title_column_1, title_column_2 = st.columns([5, 1], vertical_alignment="bottom")
+    with title_column_1:
+        st.title("Scavenger hunt admin 🕵")
+    with title_column_2:
+        if st.button(label="Reload"):
+            st.rerun()
 
+    overview_tab, questions_tab, stats_tab = st.tabs(["Overview", "Questions", "Statistics"])
     with overview_tab:
-        st.header("Overview")
-        st.write(f"Number of registered teams: {state.n_active_teams}")
-        print(state.teams)
+        ## Team statistics
+        st.subheader("Team statistics")
+        if state.n_active_teams > 0:
+            st.write(f"Number of registered teams: {state.n_active_teams}")
+            team_statistics = []
+            for team in state.teams.values():
+                current_goal = team.goal_location_name if len(team.solved) < len(game.locations) else "All locations solved"
+                team_statistics.append({
+                    "Team": team.name,
+                    # "Score": sum([score for score in team.solved.values()]),
+                    "Solved": len(team.solved),
+                    "Current goal": current_goal,
+                })
 
+            st.dataframe(team_statistics)
+        else:
+            st.write("No teams have registered yet.")
+
+        ## Puzzle statistics
         st.subheader("Puzzle statistics")
         puzzle_statistics = []
         for ix, location in enumerate(game.locations):
@@ -37,65 +61,173 @@ def scavenger_admin():
                 for team in state.teams.values()
                 if location.name in team.solved
             )
+            n_teams_correct = sum(
+                1
+                for team in state.teams.values()
+                if location.name in team.solved and team.solved[location.name] > 0
+            )
+            n_teams_incorrect = n_teams_solved - n_teams_correct
             puzzle_statistics.append({
                 "Id": ix + 1,
                 "Location": location.name,
-                "Teams solved": n_teams_solved,
-                "Teams unsolved": state.n_active_teams - n_teams_solved,
+                "Teams answered": n_teams_solved,
+                "Teams unanswered": state.n_active_teams - n_teams_solved,
+                "Teams correct": n_teams_correct,
+                "Teams incorrect": n_teams_incorrect,
             })
 
-        st.dataframe(puzzle_statistics)
+        st.dataframe(data=puzzle_statistics, height=500)
 
-        st.subheader("Team statistics")
-        team_statistics = []
-        for team in state.teams.values():
-            team_statistics.append({
-                "Team": team.name,
-                "Score": sum([score for score in team.solved.values()]),
-                "Solved": len(team.solved),
-            })
+        ## Danger zone
+        st.subheader("Danger zone")
+        st.markdown("**Enable beam-to-location for all teams.** All teams will see a beam to location checkbox when their UI refreshes. When this checkbox is enabled, you are automatically beamed to the current goal and can directly answer the question.")
 
-        st.dataframe(team_statistics)
+        def change_beam_to_location_if_state_exists(state: State, beam_to_location: bool):
+            """Change beam to location visibility."""
+            if Path(STATE_FILE).exists() and state.button_beam_to_location_visible != beam_to_location:
+                state.button_beam_to_location_visible = beam_to_location
+                state.save()
+
+        beam_to_location_button_visible = st.checkbox(
+            label="Show `beam-to-location` checkbox in user interface",
+        )
+        change_beam_to_location_if_state_exists(state, beam_to_location_button_visible)
+
+        st.markdown("---")
+        st.markdown("**Delete all team data.** This will delete all team data and reset the game. This should only be done if nobody is actually logged in as each client can restore their version of the state")
+        if st.checkbox(label="Delete all team data"):
+            if st.button(label="Confirm deletion"):
+                state_folder = Path(STATE_FILE).parent
+                for file in state_folder.glob("*"):
+                    file.unlink()
+
+                st.write("All team data has been deleted.")
+
 
     with questions_tab:
-
-        def previous_item():
-            st.session_state.index = (st.session_state.index - 1) % len(game.locations)
-
-        def next_item():
-            st.session_state.index = (st.session_state.index + 1) % len(game.locations)
-
-        col1, col2 = st.columns([1, 1])
-
-        with col1:
-            if st.button("Previous"):
-                previous_item()
-
-        with col2:
-            if st.button("Next"):
-                next_item()
-
         location: Location = game.locations[st.session_state.index]
         if not isinstance(location, Location):
             st.write("No more questions available.")
             return
 
+        col1, col2, col3 = st.columns([4, 1, 1])
+
+        with col1:
+            st.subheader(f"{st.session_state.index + 1} - {location.name}")
+
+        with col2:
+            def previous_item():
+                """Select previous item."""
+                st.session_state.index = (st.session_state.index - 1) % len(game.locations)
+
+            st.button(label="Previous", on_click=previous_item)
+
+        with col3:
+            def next_item():
+                """Select next item."""
+                st.session_state.index = (st.session_state.index + 1) % len(game.locations)
+
+            st.button(label="Next", on_click=next_item)
+
         base_question_path = Path(GAME_FILE).parent
         image_file = base_question_path / location.image
 
-        st.subheader(location.name)
         st.markdown(location.question)
         if image_file.exists():
             st.image(str(image_file), use_container_width=True)
 
         st.subheader("Answer:")
+        show_score = st.checkbox(label="Show score", value=False)
         for option in location.answer:
-            st.write(f"Option: {option.option}, Score: {option.score}")
+            if show_score:
+                st.write(f"Option: {option.option}, Score: {option.score}")
+            else:
+                st.write(f"Option: {option.option}")
 
-    with about_tab:
-        st.header("About Us")
-        st.write("Here's some information about us.")
-        st.bar_chart([10, 20, 30, 40])
+    with stats_tab:
+        ## Title
+        st.subheader("Map")
+
+        # load ndjson logging file
+        with open(LOGGING_FILE, "r") as file:
+            logs = pd.DataFrame([json.loads(line) for line in file])
+
+        # Create map
+        logs["timestamp"] = pd.to_datetime(logs["timestamp"])
+        logs_sorted = logs.sort_values(by="timestamp")
+        fig = px.line_mapbox(
+            data_frame=logs_sorted,
+            lat="latitude",
+            lon="longitude",
+            hover_name="current_goal",
+            hover_data=["timestamp"],
+            color="team_name",
+            zoom=14.5,
+            height=600,
+            color_discrete_sequence=px.colors.qualitative.Plotly,
+        )
+        fig.update_layout(
+            mapbox={
+                "style": "carto-positron",
+            },
+            margin={"r":0,"t":0,"l":0,"b":0},
+        )
+        st.plotly_chart(fig)
+        st.download_button(
+            label="Download location statistics as CSV",
+            data=logs_sorted.to_csv(),
+            file_name="dataframe.csv",
+            mime="text/csv",
+        )
+
+        ## Summary statistics
+        st.subheader("Summary statistics")
+
+        # Score
+        scores_collected = {
+            team_name: {
+                location.name: team_state.solved[location.name] if location.name in team_state.solved else 0
+                for location in game.locations
+            } for team_name, team_state in state.teams.items()
+        }
+        scores = {
+            team_name: sum(score for score in scores_collected[team_name].values())
+            for team_name in scores_collected
+        }
+
+        # Distance statistics
+        def calculate_distance(lat1, lon1, lat2, lon2):
+            if pd.isnull(lat1) or pd.isnull(lon1) or pd.isnull(lat2) or pd.isnull(lon2):
+                return 0
+            return geodesic((lat1, lon1), (lat2, lon2)).kilometers
+
+        logs_sorted["prev_latitude"] = logs_sorted["latitude"].shift()
+        logs_sorted["prev_longitude"] = logs_sorted["longitude"].shift()
+        no_beam_logs = logs_sorted[logs_sorted["beam_to_location"] == 0]
+        beam_logs = logs_sorted[logs_sorted["beam_to_location"] == 1]
+        no_beam_logs["distance"] = no_beam_logs.apply(
+            lambda row: calculate_distance(row["latitude"], row["longitude"], row["prev_latitude"], row["prev_longitude"]),
+            axis=1
+        )
+        beam_logs["distance"] = beam_logs.apply(
+            lambda row: calculate_distance(row["latitude"], row["longitude"], row["prev_latitude"], row["prev_longitude"]),
+            axis=1
+        )
+
+        summary_df = logs_sorted.groupby("team_name").apply(lambda group: pd.Series({
+            "points_clicked": no_beam_logs[no_beam_logs["team_name"] == group.name].shape[0],
+            "distance_traveled": no_beam_logs[no_beam_logs["team_name"] == group.name]["distance"].sum(),
+            "distance_beamed": beam_logs[beam_logs["team_name"] == group.name]["distance"].sum(),
+            "total_score": scores[group.name],
+        }), include_groups=False).sort_values(by="total_score", ascending=False)
+
+        st.dataframe(summary_df)
+        st.download_button(
+            label="Download summary statistics as CSV",
+            data=summary_df.to_csv(),
+            file_name="dataframe.csv",
+            mime="text/csv",
+        )
 
 
 ########
--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -25,9 +25,15 @@ def scavenger(team_name: str) -> None:
         team_name=team_name,
     )
 
+    ## Title
+    title_column_1, title_column_2 = st.columns([5, 1], vertical_alignment="bottom")
+    with title_column_1:
+        st.title("Scavenger hunt 🕵")
+    with title_column_2:
+        if st.button(label="Reload"):
+            st.rerun()
+
     ## Top section
-    ## - Team name and small description if first location (sovled = 0)
-    st.title("Scavenger hunt 🕵")
     if len(team_state.solved) == 0:
         st.write("Welcome to the Advanced Analytics scavenger hunt! Your goal is to find the hidden locations and answer the questions to score points. Questions will only be revealed when you are within a certain distance of the goal location. When a question is answered correctly, the next location will be the closest one. When answered incorrectly, the next location will be the farthest one. Answer wisely!")
 
@@ -70,6 +76,7 @@ def scavenger(team_name: str) -> None:
             longitude=location.get("longitude"),
             solved=len(team_state.solved),
             current_goal=team_state.goal_location_name,
+            beam_to_location=state.button_beam_to_location_visible,
         )
         st.markdown("---")
         st.subheader("Location and direction")
